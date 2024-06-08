b792aab90847bd048c78433f4f10905debb85cf0
narumincho <16481886+narumincho@users.noreply.github.com>
2024-06-08 14:52:18 +0900

    add path0, path1, path2, ...

--- lib/nrouter.dart
+++ lib/nrouter.dart
@@ -1,68 +1,237 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
-import 'package:result_type/result_type.dart';
 
 @immutable
-class PathSegmentParserAndBuilder<T> {
-  PathSegmentParserAndBuilder.custom({
+class ParserAndBuilder<Parsed, Raw> {
+  const ParserAndBuilder.custom({
     required this.parser,
     required this.builder,
   });
 
-  final Result<T, String> Function(String pathSegment) parser;
-  final Result<String, String> Function(T value) builder;
+  final Parsed Function(Raw raw) parser;
+  final Raw Function(Parsed parsed) builder;
 
-  static PathSegmentParserAndBuilder<T> oneOf<T>(
-      List<PathSegmentParserAndBuilder<T>> parsers) {
-    return PathSegmentParserAndBuilder<T>.custom(
+  ParserAndBuilder<NewParsed, Raw> map<NewParsed>(
+    NewParsed Function(Parsed prevParsed) parserMap,
+    Parsed Function(NewParsed newParsed) builderMap,
+  ) =>
+      ParserAndBuilder<NewParsed, Raw>.custom(
+        parser: (raw) => parserMap(parser(raw)),
+        builder: (parsed) => builder(builderMap(parsed)),
+      );
+
+  static ParserAndBuilder<Parsed, Raw> oneOf<Parsed, Raw>(
+      List<ParserAndBuilder<Parsed, Raw>> parsers) {
+    return ParserAndBuilder<Parsed, Raw>.custom(
       parser: (pathSegment) {
         for (final parser in parsers) {
-          final result = parser.parser(pathSegment);
-          if (result.isSuccess) {
-            return result;
+          try {
+            return parser.parser(pathSegment);
+          } catch (e) {
+            // ignore
           }
         }
-        return Failure('No parser matched $pathSegment');
+        throw Exception('No parser matched $pathSegment');
       },
       builder: (value) {
         for (final parser in parsers) {
-          final result = parser.builder(value);
-          if (result.isSuccess) {
-            return result;
+          try {
+            return parser.builder(value);
+          } catch (e) {
+            // ignore
           }
         }
-        throw ArgumentError('builder error $value');
+        throw Exception('builder error $value');
       },
     );
   }
 
-  static final integer = PathSegmentParserAndBuilder<int>.custom(
-    parser: (pathSegment) => switch (int.tryParse(pathSegment)) {
-      null => Failure('Expected integer, got $pathSegment'),
-      final value => Success(value),
-    },
-    builder: (value) => Success(value.toString()),
+  static final integer = ParserAndBuilder<int, String>.custom(
+    parser: (pathSegment) => int.parse(pathSegment),
+    builder: (value) => value.toString(),
   );
 
-  static PathSegmentParserAndBuilder<Null> keyword(String name) =>
-      PathSegmentParserAndBuilder<Never>.custom(
+  static ParserAndBuilder<Null, String> keyword(String name) =>
+      ParserAndBuilder<Null, String>.custom(
         parser: (pathSegment) {
           if (pathSegment == name) {
-            return Success<Never, String>((() {
-              throw Exception('Unreachable');
-            })());
+            return null;
           }
-          return Failure<Never, String>('Expected $name, got $pathSegment');
+          throw Exception('Expected $name, got $pathSegment');
         },
-        builder: (_) => Success(name),
+        builder: (_) => name,
       );
+
+  static final ParserAndBuilder<Null, IList<String>> path0 =
+      ParserAndBuilder<Null, IList<String>>.custom(
+    parser: (pathSegments) {
+      if (pathSegments.isNotEmpty) {
+        throw Exception('Expected 0 path segment, got $pathSegments');
+      }
+      return null;
+    },
+    builder: (_) => const IListConst([]),
+  );
+
+  static ParserAndBuilder<S0, IList<String>> path1<S0>(
+          ParserAndBuilder<S0, String> segment0) =>
+      ParserAndBuilder<S0, IList<String>>.custom(
+        parser: (pathSegments) {
+          return switch (pathSegments.toList()) {
+            [final pathSegment0] => segment0.parser(pathSegment0),
+            _ => throw Exception('Expected 1 path segment, got $pathSegments'),
+          };
+        },
+        builder: (value) => IList([segment0.builder(value)]),
+      );
+
+  static ParserAndBuilder<(S0, S1), IList<String>> path2<S0, S1>(
+    ParserAndBuilder<S0, String> segment0,
+    ParserAndBuilder<S1, String> segment1,
+  ) =>
+      ParserAndBuilder<(S0, S1), IList<String>>.custom(
+        parser: (pathSegments) {
+          return switch (pathSegments.toList()) {
+            [final pathSegment0, final pathSegment1] => (
+                segment0.parser(pathSegment0),
+                segment1.parser(pathSegment1)
+              ),
+            _ => throw Exception('Expected 2 path segment, got $pathSegments'),
+          };
+        },
+        builder: (value) => IList([
+          segment0.builder(value.$1),
+          segment1.builder(value.$2),
+        ]),
+      );
+
+  static ParserAndBuilder<(S0, S1, S2), IList<String>> path3<S0, S1, S2>(
+    ParserAndBuilder<S0, String> segment0,
+    ParserAndBuilder<S1, String> segment1,
+    ParserAndBuilder<S2, String> segment2,
+  ) =>
+      ParserAndBuilder<(S0, S1, S2), IList<String>>.custom(
+        parser: (pathSegments) {
+          return switch (pathSegments.toList()) {
+            [final pathSegment0, final pathSegment1, final pathSegment2] => (
+                segment0.parser(pathSegment0),
+                segment1.parser(pathSegment1),
+                segment2.parser(pathSegment2),
+              ),
+            _ => throw Exception('Expected 3 path segment, got $pathSegments')
+          };
+        },
+        builder: (value) => IList([
+          segment0.builder(value.$1),
+          segment1.builder(value.$2),
+          segment2.builder(value.$3),
+        ]),
+      );
+
+  static ParserAndBuilder<(S0, S1, S2, S3), IList<String>>
+      path4<S0, S1, S2, S3>(
+    ParserAndBuilder<S0, String> segment0,
+    ParserAndBuilder<S1, String> segment1,
+    ParserAndBuilder<S2, String> segment2,
+    ParserAndBuilder<S3, String> segment3,
+  ) =>
+          ParserAndBuilder<(S0, S1, S2, S3), IList<String>>.custom(
+            parser: (pathSegments) {
+              return switch (pathSegments.toList()) {
+                [
+                  final pathSegment0,
+                  final pathSegment1,
+                  final pathSegment2,
+                  final pathSegment3
+                ] =>
+                  (
+                    segment0.parser(pathSegment0),
+                    segment1.parser(pathSegment1),
+                    segment2.parser(pathSegment2),
+                    segment3.parser(pathSegment3),
+                  ),
+                _ =>
+                  throw Exception('Expected 4 path segment, got $pathSegments')
+              };
+            },
+            builder: (value) => IList([
+              segment0.builder(value.$1),
+              segment1.builder(value.$2),
+              segment2.builder(value.$3),
+              segment3.builder(value.$4),
+            ]),
+          );
+
+  static ParserAndBuilder<(S0, S1, S2, S3, S4), IList<String>>
+      path5<S0, S1, S2, S3, S4>(
+    ParserAndBuilder<S0, String> segment0,
+    ParserAndBuilder<S1, String> segment1,
+    ParserAndBuilder<S2, String> segment2,
+    ParserAndBuilder<S3, String> segment3,
+    ParserAndBuilder<S4, String> segment4,
+  ) =>
+          ParserAndBuilder<(S0, S1, S2, S3, S4), IList<String>>.custom(
+            parser: (pathSegments) {
+              return switch (pathSegments.toList()) {
+                [
+                  final pathSegment0,
+                  final pathSegment1,
+                  final pathSegment2,
+                  final pathSegment3,
+                  final pathSegment4
+                ] =>
+                  (
+                    segment0.parser(pathSegment0),
+                    segment1.parser(pathSegment1),
+                    segment2.parser(pathSegment2),
+                    segment3.parser(pathSegment3),
+                    segment4.parser(pathSegment4),
+                  ),
+                _ =>
+                  throw Exception('Expected 5 path segment, got $pathSegments')
+              };
+            },
+            builder: (value) => IList([
+              segment0.builder(value.$1),
+              segment1.builder(value.$2),
+              segment2.builder(value.$3),
+              segment3.builder(value.$4),
+              segment4.builder(value.$5),
+            ]),
+          );
 }
 
-final routeList = [
-  createRoute2(
-    ((
-      PathSegmentParserAndBuilder.keyword('user'),
-      PathSegmentParserAndBuilder.integer
-    )),
-    (context, param) => 'Widget',
-  ),
-];
+@immutable
+sealed class Sample {
+  const Sample();
+}
+
+@immutable
+class SampleRoot implements Sample {
+  const SampleRoot();
+}
+
+@immutable
+class SampleSetting implements Sample {
+  const SampleSetting();
+}
+
+@immutable
+class SampleUser implements Sample {
+  const SampleUser(this.id);
+
+  final int id;
+}
+
+final sample = ParserAndBuilder.oneOf<Sample, IList<String>>([
+  ParserAndBuilder.path0.map<Sample>((_) => const SampleRoot(), (_) => null),
+  ParserAndBuilder.path1(ParserAndBuilder.keyword('settings').map(
+    (_) => const SampleSetting(),
+    (_) => null,
+  )),
+  ParserAndBuilder.path2(
+    ParserAndBuilder.keyword('user'),
+    ParserAndBuilder.integer,
+  ).map<SampleUser>((prevParsed) => SampleUser(prevParsed.$2),
+      (newParsed) => (null, newParsed.id)),
+]);
